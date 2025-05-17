22a75b5609b3df747e9d377c730a81487c373090
choseohye <hyesaye@icloud.com>
2025-05-17 14:59:39 +0900

    OVERLAY

--- src/main/java/com/withcare/member/controller/MemberController.java
+++ src/main/java/com/withcare/member/controller/MemberController.java
@@ -1,4 +1,12 @@
 package com.withcare.member.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
+	
+	Logger log = LoggerFactory.getLogger(getClass());
+
 }
