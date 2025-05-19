ea0799834ebe79a07bf4aa064b34614890c7eb69
oneidsin <oneidsin@naver.com>
2025-05-19 15:35:30 +0900

    배지 조회, 배지 획득 여부 확인, 대표 배지 설정

--- src/main/java/com/withcare/profile/dao/BadgeDAO.java
+++ src/main/java/com/withcare/profile/dao/BadgeDAO.java
@@ -10,5 +10,13 @@ public interface BadgeDAO {
 
     List<Map<String, Object>> getMemberBadgeList(String id);
 
-    Map<String, Object> getMemberAcquiredBadge(String id);
+    List<Map<String, Object>> getMemberAcquiredBadge(String id);
+
+    int isBadgeAcquired(String id, int bdg_idx);
+
+    void insertAcquiredBadge(String id, int bdg_idx);
+
+    int clearAllBadgeSym(String id);
+
+    int updateBadgeSym(String id, int bdg_idx);
 }
