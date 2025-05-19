1ee43f0bbc30fb1efb305752791c798684c2f846
oneidsin <oneidsin@naver.com>
2025-05-19 11:03:42 +0900

    badge 리스트 불러오기 기능 추가

--- src/main/java/com/withcare/profile/service/BadgeService.java
+++ src/main/java/com/withcare/profile/service/BadgeService.java
@@ -1,12 +1,21 @@
 package com.withcare.profile.service;
 
+import com.withcare.profile.dao.BadgeDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class BadgeService {
     Logger log = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    BadgeDAO dao;
 
+    public Map<String, Object> getMemberBadgeList(String id) {
+        return dao.getMemberBadgeList(id);
+    }
 }
