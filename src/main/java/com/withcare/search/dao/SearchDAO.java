280dda2eaf390c5a0d591be3505424475a110edb
sang_yoon <dltkddbsfl@gmail.com>
2025-05-19 18:02:50 +0900

    프로필완료

--- src/main/java/com/withcare/search/dao/SearchDAO.java
+++ src/main/java/com/withcare/search/dao/SearchDAO.java
@@ -1,8 +1,16 @@
 package com.withcare.search.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.withcare.post.dto.PostDTO;
 
 @Mapper
 public interface SearchDAO {
 
-}
+	   void insertSearchHistory(String userId, String keyword);
+	    List<PostDTO> searchPostsByKeyword(String userId, String keyword);
+	}
+
+
